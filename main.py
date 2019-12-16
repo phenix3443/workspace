60c8333cb41bda4d169a551919d9ae43bb436951
phenix3443 <phenix3443@gmail.com>
2019-12-17 01:18:36 +0800

    整理脚本

--- main.py
+++ main.py
@@ -1,78 +1,88 @@
 # -*- coding:utf-8 -*-
-PREINSTALL = {
-    "apt" :{
-    "zeal":["add-apt-repository ppa:zeal-developers/ppa"],
-    "albert":["curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -",
-"sudo sh -c \"echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list\""]     
-}
-   
-}
 
 
 devtools = {
-    "common":[
-              "zssh",
-              "sshfs",
-              "tmux",
-                            "curl",
-              "zeal",
-              "bash-completion",
-              "build-essential",
-              "automake",
-              "autoconf",
-              "libtool",
-              "gcc",
-              "cmake",
-              # cxx
-
-              
-]
+    "apt-pre": ["add-apt-repository ppa:zeal-developers/ppa"],
+    "common": ["zssh", "sshfs", "tmux", "curl", "zeal", "git"],
+    "apt": [],
+    "yum": [],
+    "brew": [],
 }
 
-program_language = {
-    "cxx":[
-                         "cppcheck",
-              "clang",
-              "clang-format",
-           
-],
-    "lua":[
-           "lua", "luarocks",
-]
-    }
+program = {
+    "bash": {"common": ["bash-completion",]},
+    "cxx": {
+        "common": [
+            "cppcheck",
+            "clang",
+            "clang-format",
+            "cmake",
+            "build-essential",
+            "automake",
+            "autoconf",
+            "libtool",
+            "gcc",
+        ]
+    },
+    "lua": {
+        "common": ["lua", "luarocks"],
+        "PM": [
+            "luacheck",
+            "lanes",
+            "checks",
+            "Formatter",
+            "ldoc",
+            "lua-discount",
+            "lunamark",
+            "lua-cjson",
+            "penlight",
+        ],
+        "speical": [],
+    },
+    "python3": {
+        "common": ["python3", "python3-pip"],
+        "PM": ["pipenv", "ipython", "yapf", "pylint", "black"],
+    },
+    "golang": {
+        "common": ["golang"],
+        "PM": [],
+        "specail": ["GO111MODULE=on go get golang.org/x/tools/gopls@latest"],
+    },
+    "js": {"common": ["npm"]},
+    "markdown": {
+        "common": ["markdown"],
+        "special": [
+            "npm --registry https://registry.npm.taobao.org install -g livedown"
+        ],
+    },
+}
 
 applications = {
-    "common":[
-              "shutter",
-              "shadowsocks-libev",
-              "proxychains4",
-              "albert",
-              "alacarte",
-              "aria2",
-              "chromium-browser",
-              "okular",
-              "smplayer",
-            
-              ],
-    "apt":[]
+    "apt-pre": [
+        "curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -",
+        "sudo sh -c \"echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list\"",
+    ],
+    "common": [
+        "shutter",
+        "shadowsocks-libev",
+        "proxychains4",
+        "albert",
+        "alacarte",
+        "aria2",
+        "chromium-browser",
+        "okular",
+        "smplayer",
+        "silversearcher-ag",
+    ],
+    "apt": [],
+    "yum": [],
+    "brew": [],
 }
 
 
-POSTINSTALL= {
-    "common":{
-        "shadowsocks":["./do-config.sh local"],
-        "proxychains4":["mkdir ~/.proxychains && cp dotfiles/_proxychains/proxychains.conf ~/.proxychains"],
-        "python3":["pip3 install black"]
-        
-        },
-        
-
-    "apt":{
-
-}
-}
 def main():
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
