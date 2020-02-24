fbefd01c29b396a5a267647e83ecd4ac3f3bb459
phenix3443 <phenix3443@gmail.com>
2020-02-24 23:24:47 +0800

    优化代码

--- main.py
+++ main.py
@@ -1,71 +1,23 @@
 # -*- coding:utf-8 -*-
 
+import logging
 
-devtools = {
-    "apt-pre": ["add-apt-repository ppa:zeal-developers/ppa"],
-    "common": ["zssh", "sshfs", "tmux", "curl", "zeal", "git"],
-    "apt": [],
-    "yum": [],
-    "brew": [],
-}
+AptPPA = [
+    "add-apt-repository ppa:zeal-developers/ppa",
+    "curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -",
+    "sudo sh -c \"echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list\"",
+    "apt update",  # 始终放在最后
+]
 
-program = {
-    "bash": {
-        "common": ["bash-completion"],
-        "special": ["npm i -g bash-language-server"],
-    },
-    "cxx": {
-        "common": [
-            "cppcheck",
-            "clang",
-            "clang-format",
-            "cmake",
-            "build-essential",
-            "automake",
-            "autoconf",
-            "libtool",
-            "gcc",
-        ]
-    },
-    "lua": {
-        "common": ["lua", "luarocks"],
-        "PM": [
-            "luacheck",
-            "lanes",
-            "checks",
-            "Formatter",
-            "ldoc",
-            "lua-discount",
-            "lunamark",
-            "lua-cjson",
-            "penlight",
-        ],
-        "speical": ["sudo npm install --global lua-fmt"],
-    },
-    "python3": {
-        "common": ["python3", "python3-pip"],
-        "PM": ["pipenv", "ipython", "yapf", "pylint", "black"],
-    },
-    "golang": {
-        "common": ["golang"],
-        "PM": [],
-        "specail": ["GO111MODULE=on go get golang.org/x/tools/gopls@latest"],
-    },
-    "js": {"common": ["npm"]},
-    "markdown": {
-        "common": ["markdown"],
-        "special": [
-            "npm --registry https://registry.npm.taobao.org install -g livedown"
-        ],
-    },
-}
-
-applications = {
-    "apt-pre": [
-        "curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -",
-        "sudo sh -c \"echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list\"",
-    ],
-    "common": [
+APT = {
+    "app": [
+        "zssh",
+        "sshfs",
+        "tmux",
+        "curl",
+        "zeal",
+        "git",
+        "bash-completion",
         "shutter",
         "shadowsocks-libev",
         "proxychains4",
@@ -76,16 +28,120 @@ applications = {
         "okular",
         "smplayer",
         "silversearcher-ag",
+        "emacs",
     ],
-    "apt": [],
-    "yum": [],
-    "brew": ["npm", "node"],
-    "brew-cask": ["shadowsocksx-ng", "shadowsocksx-ng", "iina", "telegram"],
+    "cxx": [
+        "cppcheck",
+        "clang",
+        "clang-format",
+        "cmake",
+        "build-essential",
+        "automake",
+        "autoconf",
+        "libtool",
+        "gcc",
+    ],
+    "lua": ["lua", "luarocks"],
+    "python": ["python3", "python3-pip"],
+    "golang": ["golang"],
+    "js": ["npm", "node"],
+    "markdown": ["markdown"],
 }
+
+YUM = {
+    "app": [],
+    "cxx": [],
+    "lua": [],
+    "python": [],
+    "golang": [],
+    "js": [],
+}
+
+Brew = {
+    "app": [
+        "zssh",
+        "tmux",
+        "the_silver_searcher",
+        "proxychains-ng",
+        "sshfs",
+        "markdown",
+    ],
+    "cxx": [],
+    "lua": ["lua", "luarocks"],
+    "python": ["python3"],
+    "golang": ["golang"],
+    "js": ["npm", "node"],
+}
+
+BrewCask = [
+    "dash",
+    "shadowsocksx-ng",
+    "chromium",
+    "iina",
+    "emacs",
+]
+
+
+NPM = [
+    "livedown",
+    "bash-language-server",
+    "lua-fmt",
+]
+
+LuaRocks = [
+    "luacheck",
+    "lanes",
+    "checks",
+    "Formatter",
+    "ldoc",
+    "lua-discount",
+    "lunamark",
+    "lua-cjson",
+    "penlight",
+]
+
+
+Pip = ["pipenv", "ipython", "yapf", "pylint", "black"]
+
+GO = ["GO111MODULE=on go get golang.org/x/tools/gopls@latest"]
 
 
 def main():
-    pass
+    logging.basicConfig(level=logging.DEBUG)
+
+    for s in AptPPA:
+        logging.debug(s)
+
+    for (k, v) in APT.items():
+        if v:
+            logging.debug("apt install -y {}".format(" ".join(v)))
+
+    for (k, v) in YUM.items():
+        if v:
+            logging.debug("yum install -y {}".format(" ".join(v)))
+
+    for (k, v) in Brew.items():
+        if v:
+            logging.debug("brew install {}".format(" ".join(v)))
+
+    if BrewCask:
+        logging.debug("brew cask install {}".format(" ".join(BrewCask)))
+
+    if NPM:
+        logging.debug(
+            "npm --registry https://registry.npm.taobao.org  install -g {}".format(
+                " ".join(NPM)
+            )
+        )
+
+    if LuaRocks:
+        logging.debug("luarocks install {}".format(" ".join(LuaRocks)))
+
+    if Pip:
+        logging.debug("pip3 install {}".format(" ".join(Pip)))
+
+    if GO:
+        logging.debug("go get {}".format(" ".join(GO)))
 
 
 if __name__ == "__main__":
