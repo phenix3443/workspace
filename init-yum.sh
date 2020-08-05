a9ba10b1da1670425a2ece15178e6363476c00a6
lsl <lsl@MacBook-Pro.local>
2020-08-05 20:46:44 +0800

    add new

--- init-yum.sh
+++ init-yum.sh
@@ -4,3 +4,6 @@ sudo yum install redhat-lsb-core
 
 echo "EPEL"
 sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+
+sudo yum install - gcc gcc-c++ automake autoconf libtool make
