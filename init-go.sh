b5dc764906222b1e132f16e2b24e3033150f510c
phenix3443 <phenix3443@gmail.com>
2020-09-05 23:42:34 +0800

    添加dlv

--- init-go.sh
+++ init-go.sh
@@ -3,4 +3,5 @@ echo "export GOPROXY=https://goproxy.cn" >> ~/.profile && \
 source ~/.profile && \
 GO111MODULE=on go get golang.org/x/tools/gopls@latest && \
 GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt && \
-    go get github.com/jessfraz/dockfmt
+    go get github.com/jessfraz/dockfmt && \
+go get github.com/go-delve/delve/cmd/dlv
